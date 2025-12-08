294aa0c329033e276e1b54a4699c5f6b6d2d307b
supra <alexusenzoroa@gmail.com>
2025-12-08 17:44:22 +0800

    commit test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
+        // new comment
         try {
             Database.getConn().close();
         } catch (SQLException e) {
